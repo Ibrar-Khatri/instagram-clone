a2e5abfeba0d84a08e3b41740c783641c22f7434
Ibrar-Khatri <khatriibrar116@gmail.com>
2022-05-17 12:42:44 +0500

    lambda configured

--- amplify/backend/function/InstagramPostConfirmation/src/custom.js
+++ amplify/backend/function/InstagramPostConfirmation/src/custom.js
@@ -1,9 +1,67 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const env = process.env.ENV;
+const AppsyncID = process.env.API_INSTAGRAM_GRAPHQLAPIIDOUTPUT;
+const TableName = `User-${AppsyncID}-${env}`; //TableName-AppsyncID-env
+
+const userExists = async id => {
+  const params = {
+    TableName,
+    Key: id,
+  };
+  try {
+    const response = await docClient.get(params).promise();
+    return !!response?.Item;
+  } catch (e) {
+    return false;
+  }
+};
+const saveUser = async user => {
+  const date = new Date();
+  const dateStr = date.toISOString();
+  const timestamp = date.getTime();
+  const Item = {
+    ...user,
+    __typename: 'User',
+    createdAt: dateStr,
+    updatedAt: dateStr,
+    _lastChangedAt: timestamp,
+    _version: 1,
+  };
+  const params = {
+    TableName,
+    Item,
+  };
+  try {
+    await docClient.put(params).promise();
+  } catch (e) {
+    console.log('🚀 ~ e', e);
+  }
+};
+
 exports.handler = async (event, context) => {
-  // insert code to be executed by your lambda trigger
-  console.log('Hey Lambda function is triggered');
+  console.log('Lambda function triggered');
   console.log('🚀 ~ event', event);
+  if (!event?.request?.userAttributes) {
+    console.log('No user data available');
+    return;
+  }
+  const {sub, name, email} = event?.request?.userAttributes;
+  const newUser = {
+    id: sub,
+    name,
+    email,
+  };
+  if (!(await userExists(newUser.id))) {
+    await saveUser(newUser);
+    console.log(`User ${newUser.id} has been saved to the database`);
+  } else {
+    console.log(`User ${newUser.id} already exists`);
+  }
   return event;
 };
